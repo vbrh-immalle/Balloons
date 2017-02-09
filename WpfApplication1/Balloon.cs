98fc44e6d45503242bd4d3422106bfb4b80a2b60
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2017-02-09 13:37:19 +0100

    Cleaned up code, added comments

--- WpfApplication1/Balloon.cs
+++ WpfApplication1/Balloon.cs
@@ -10,25 +10,47 @@ using System.Windows.Controls;
 
 namespace WpfApplication1
 {
-    // ENCAPSULATIE
-
     class Balloon
     {
+        // First we define the PRIVATE parts of the Balloon.
+
         private int x = 10;
         private int y = 10;
         private int diameter = 10;
+
+        const int growAmount = 10;
+        const int moveAmount = 10;
 
         Ellipse ellipse = new Ellipse();
         Brush strokeBrush = Brushes.Red;
         Brush bgBrush = new LinearGradientBrush(Colors.Red, Colors.Pink, 90);
 
-        public Balloon(Canvas canvas, int diameter)
+        /*
+         * This method uses the class variables x, y and diameter
+         * to update the WPF-controls included in this class.
+         */
+        private void UpdateBalloon(Canvas canvas)
         {
-            this.diameter = diameter;
-
-            UpdateBalloon(canvas);
+            ellipse.Width = diameter;
+            ellipse.Height = diameter;
+            ellipse.Margin = new Thickness(x, y, 0, 0);
+            ellipse.Stroke = strokeBrush;
+            ellipse.Fill = bgBrush;
         }
 
+
+        // BELOW this point, you will find the PUBLIC interface to the Balloon
+
+        /*
+         * This constructor uses another constructor to specify default values for
+         * height and xpos.
+         */ 
+        public Balloon(Canvas canvas, int diameter) : this(canvas, diameter, 10, 10)
+        { }
+
+        /*
+         * This constructor allows choosing the diameter, height and xpos of the balloon
+         */ 
         public Balloon(Canvas canvas, int diameter, int height, int xpos)
         {
             this.diameter = diameter;
@@ -36,32 +58,19 @@ namespace WpfApplication1
             y = height;
 
             UpdateBalloon(canvas);
-        }
-        
-        /*
-         * This method uses the class variables x, y and diameter
-         * to update the WPF-controls included in this class.
-         */
-        void UpdateBalloon(Canvas canvas)
-        {
-            ellipse.Width = diameter;
-            ellipse.Height = diameter;
-            ellipse.Margin = new Thickness(x, y, 0, 0);
-            ellipse.Stroke = strokeBrush;
-            ellipse.Fill = bgBrush;
             canvas.Children.Add(ellipse);
         }
 
         public void Grow()
         {
-            diameter += 10;
+            diameter += growAmount;
             ellipse.Width = diameter;
             ellipse.Height = diameter;
         }
 
         public void Move()
         {
-            y -= 10;
+            y -= moveAmount;
             ellipse.Margin = new Thickness(x, y, 0, 0);
         }
 
